23c67518bf482c04320e0d315dcff42a0a0f2b39
Kevenson Leger <50429391+lkevenson@users.noreply.github.com>
2024-03-26 15:23:40 -0400

    [Section 5] : MainChallege v2

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -13,20 +13,19 @@ public class MainChallenge {
 
         int finalScore = score;
 
-        if(gameOver == true){
+        if (gameOver == true) {
             finalScore += (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
 
 
-        boolean newGameOver = true;
-        int newScore = 10_000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
-       int  newFinalScore = newScore;
-        if(newGameOver == true){
-            newFinalScore += (newLevelCompleted * newBonus);
-            System.out.println("Ypur final score was " + newFinalScore);
+        score = 10_000;
+        levelCompleted = 8;
+        bonus = 200;
+        finalScore = score;
+        if (gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Ypur final score was " + finalScore);
         }
     }
 }
