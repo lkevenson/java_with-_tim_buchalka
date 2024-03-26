007d29089a4fc1d2cafc1a0f96798d63a35359cb
Kevenson Leger <50429391+lkevenson@users.noreply.github.com>
2024-03-26 14:30:54 -0400

    Codeblock with simple IF-ELSE

--- IfKeywordAndCodeBlocks/src/Main.java
+++ IfKeywordAndCodeBlocks/src/Main.java
@@ -7,8 +7,10 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score == 5000){
-            System.out.println("Your score was 5000");
-        }
+       if(score < 5000){
+           System.out.println("Your score was less than 5000");
+       } else {
+           System.out.println("get here");
+       }
     }
 }
